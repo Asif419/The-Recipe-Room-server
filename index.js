d2eec058b8c4a000df1251d8769860859c8925cb
Asif419 <asshah419@gmail.com>
2023-05-02 07:13:56 +0600

    response for chef and chef id made

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
+const chefs = require('./data/chef.json')
 
 app.use(cors());
 
@@ -9,8 +10,15 @@ app.get('/', (req, res) => {
   res.send('the-recipe-room');
 });
 
-app.get('/news', (req, res) => {
-  res.send('okay!!!the-recipe-room');
+app.get('/chefs', (req, res) => {
+  res.send(chefs);
+});
+
+app.get('/chefs/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  console.log(id)
+  const selectedChef = chefs.find(c => c.chef_id === id);
+  res.send(selectedChef);
 })
 
 app.listen(port, () => {
