49db6c4cd2a8b9431fcbd27f8f6c0313875acae3
Asif419 <asshah419@gmail.com>
2023-05-02 06:06:47 +0600

    foundation and cors installation

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000
+
+app.use(cors());
+
+app.get('/', (req, res) => {git branch -M main
+  res.send('the-recipe-room');
+})
+
+app.listen(port, () => {
+  console.log(`the recipe room is running on ${port}`)
+})
